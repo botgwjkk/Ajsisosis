444113024ebef5563405261f8daf1d13b611eec2
botgwjkk <allresultyanznesia@gmail.com>
2025-11-11 21:20:12 +0700

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,8 @@
+module.exports = {
+  TOKEN: '8214397078:AAFBkh_H-wTwtvjg-bBlmpOVOL2GD15YoU0',   // Ganti token bot kamu
+  OWNER_ID: 6246550447, // ganti id disini
+  OWNER_CONTACT_URL: 'https://t.me/Jeckk88',
+  MIN_GROUP_FOR_PREMIUM: 2, // minimal grub
+  PREMIUM_DAYS_ON_JOIN: 1, // minimal premium
+  BANNER_FILE_ID: 'https://i.ibb.co/zVTmKd8z/images-1.jpg' // ganti foto disini
+};
